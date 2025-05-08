f7245269ec660bce84d7cac361830dff93103fdd
Jakub Baran <jakub.baran.b@gmail.com>
2025-05-08 07:53:22 +0200

    feat: enhance chat functionality by including message history and file handling

--- src/collections/Notes/endpoints/chat.ts
+++ src/collections/Notes/endpoints/chat.ts
@@ -1,5 +1,4 @@
 import { generateChatResponse } from '@/services/ollama'
-import { convertLexicalToPlaintext } from '@payloadcms/richtext-lexical/plaintext'
 import { Message } from 'ollama'
 import { Endpoint } from 'payload'
 
@@ -26,43 +25,31 @@ export const noteChatEndpoint: Endpoint = {
       return Response.json({ error: 'Invalid JSON in request body' }, { status: 400 })
     }
 
-    const { message } = body as {
+    const { message, history } = body as {
       message: string
+      history?: Message[]
     }
 
     if (!message) {
       return Response.json({ error: 'Message is required' }, { status: 400 })
     }
 
-    const history = (note.chatMessages || []) as Message[]
-
     try {
       const response = await generateChatResponse(message, [
         {
           role: 'system',
-          content: `You are a helpful assistant. You are chatting about the note titled "${note.title}".`,
+          content: `
+You are a helpful assistant. You are chatting about the note titled "${note.title}". 
+If you asked for ask questions about the note, you should ask them in a way that is relevant to the note's content.
+Every question should be in own messages. After each question, wait for the user to respond and check if the user answered the question correctly. 
+          `.trim(),
         },
         {
           role: 'system',
           content: `Note content: ${note.content}`,
         },
-        // ...history,
+        ...(history || []),
       ])
-
-      await req.payload.update({
-        collection: 'notes',
-        id,
-        data: {
-          chatMessages: [
-            ...history,
-            {
-              role: 'user',
-              content: message,
-            },
-            ...response.messages,
-          ],
-        },
-      })
 
       return Response.json({ response })
     } catch (error) {
