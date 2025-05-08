b3dd62e00fd4cd3b9e7c282031aa8f941dc4877a
Jakub Baran <jakub.baran.b@gmail.com>
2025-05-08 05:27:58 +0200

    feat: access control in collections

--- src/collections/Notes/index.ts
+++ src/collections/Notes/index.ts
@@ -1,10 +1,52 @@
-import type { CollectionConfig } from 'payload'
+import type { Access, CollectionConfig } from 'payload'
 import { noteChatEndpoint } from './endpoints/chat'
 import { generateNoteEndpoint } from './endpoints/generate'
+import { Note } from '@/payload-types'
+
+const noteAccess: Access<Note> = ({ data, req: { user } }) => {
+  if (!user) return false
+
+  if (!data)
+    return {
+      or: [
+        {
+          author: {
+            equals: user.id,
+          },
+          status: {},
+        },
+        {
+          author: {},
+          status: {
+            equals: 'public',
+          },
+        },
+      ],
+    }
+
+  if (data.status !== 'private') return true
+  if (data.author === user.id) return true
+
+  return false
+}
+
+const authorAccess: Access<Note> = ({ req: { user } }) => {
+  if (!user) return false
+  return {
+    author: {
+      equals: user.id,
+    },
+  }
+}
 
 export const Notes: CollectionConfig = {
   slug: 'notes',
   endpoints: [noteChatEndpoint, generateNoteEndpoint],
+  access: {
+    read: noteAccess,
+    update: authorAccess,
+    delete: authorAccess,
+  },
   fields: [
     {
       name: 'title',
--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,10 +1,20 @@
-import type { CollectionConfig } from 'payload'
+import { User } from '@/payload-types'
+import type { Access, CollectionConfig } from 'payload'
+
+const selfAccess: Access<User> = ({ data, req: { user } }) => {
+  if (!user || !data) return false
+  if (user.id === data.id) return true
+  return false
+}
 
 export const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
   access: {
     create: () => true,
+    update: selfAccess,
+    delete: selfAccess,
+    admin: () => process.env.NODE_ENV === 'development',
   },
   fields: [],
 }
